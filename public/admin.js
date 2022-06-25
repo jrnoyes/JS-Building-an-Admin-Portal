9472edb14bb2b1a1fc9ef56beea6b37b7a9baaa9
Jason Noyes <jrnoyes1@gmail.com>
2022-06-24 20:28:46 -0500

    final commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,44 @@
+async function admin() {
+const bookListResponse = await fetch('http://localhost:3001/listBooks');
+const bookList = await bookListResponse.json();
 
+const ul = document.createElement('ul');
+
+bookList.forEach(book => {
+
+    const li = document.createElement('li')
+    li.textContent = book.title;
+
+    const textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.value = book.quantity;
+    li.append (textInput);
+
+   const button = document.createElement('input')
+    button.type = 'button';
+    button.value = 'Save';
+    button.addEventListener('click', async () => {
+       await fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                ...book,
+                quantity: textInput.value,
+            }),
+        });
+    });
+    
+    li.append(button);
+
+    ul.append(li);
+});
+
+    const root = document.querySelector('#root');
+    root.append(ul);
+
+}
+
+admin();
 // Your Code Here
